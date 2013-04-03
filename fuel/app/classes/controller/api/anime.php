e51ac60de936acd6d24f2f4739fca04316b7442b
otiai10 <otiai10@gmail.com>
2013-04-03 16:37:15 +0900

    api routing

---
+++ fuel/app/classes/controller/api/anime.php
@@ -0,0 +1,9 @@
+<?php
+
+class Controller_Api_Anime extends Controller_Template
+{
+	public function action_index()
+	{
+    Response::redirect('/');
+	}
+}
--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -4,5 +4,6 @@ return array(
 	'_404_'   => 'top/404',    // The main 404 route
 	
 	'stream(/:page)?' => 'stream/index',
+  'anime'           => 'api/anime/index',
 	'hello(/:name)?' => array('top/hello', 'name' => 'hello'),
 );
